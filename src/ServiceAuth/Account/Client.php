4de04edb427cd0d430736966b59d9bfb18d10263
Keith Pincombe <pincombe@gmail.com>
2013-01-17 12:24:13 +0000

    made transmit calls private

--- src/ServiceAuth/Account/Client.php
+++ src/ServiceAuth/Account/Client.php
@@ -9,16 +9,14 @@ class Client extends \Transmit\Client
 
 	public function fetchByKey($key)
 	{
-		$response = $this->get(sprintf('/auth/%s', $key));
+		$response = $this->_get(sprintf('/auth/%s', $key));
 		return json_decode($response);
 	}
 
 	public function fetch($id)
 	{
-		$response = $this->get(sprintf('/account/%d', $account));
+		$response = $this->_get(sprintf('/account/%d', $account));
 		return json_decode($response);
 	}
-
-
 
 }
